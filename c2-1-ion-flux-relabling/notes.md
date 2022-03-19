b6f57432b64e686d5b920381f4d54b9336978f30
Nick Chmela <nickchmela@gmail.com>
2022-03-19 15:50:56 -0400

    initialized c2 part 1

--- c2-1-ion-flux-relabling/notes.md
+++ c2-1-ion-flux-relabling/notes.md
@@ -1,12 +1,101 @@
-## Identify
+## 1. Identify
 
+Go to youtube and understand what a binary tree is before tying to understand the question. 
+
+Binary tree -> each child has 2 nodes at most
+
+**Perfect Binary Tree** -> Each level is filled, and each node has 0 or 2 children. 
+
+Balanced trees = better performance = the left and right tree of each node cannot be more than 1 height apart!
+
+MATHS
+
+leaf nodes = nodes w/ 2 children + 1
+
+max_nodes_at_level = 2^(L-1) where L is the level relative to the top. 
+
+e.g.
+
+1 = 2^(1-1)
+
+2 = 2^(2-1)
+
+4 = 2^(3-1)
+
+etc...
+
+max_nodes = 2^(H + 1) -1  where H is tree height
+
+1 = 2^(0 + 1) -1
+
+3 = 2^(1 + 1) -1
+
+7 = 2^(2 + 1) -1
+
+15 = 2^(3 + 1) -1
+
+etc...
+
+
+**Binary Tree Uses:** Binary tree search, Binary heap
 ### problem
 
+
+
 ### benefit
-
+The doomsday device is working again, so we can continue to be EVIL. 
 ### goal
+Rebuild the ion flux converter
 
 
-##  Research
 
 
+
+##  2. Research
+
+
+**Past Solution**
+1. Perform post-order traversal of the the tree of *converters*
+2. Label each *converter* with it's order in the traversal, starting with 1. 
+
+e.g. 7 converter tree
+7
+3 6 
+1 2 4 5
+
+
+
+
+**Binary Trees**
+
+**post-order-traversal**
+
+**perfect binary trees**
+
+
+## 3. Psuedocode
+
+
+
+def solution(h)
+
+
+
+
+
+
+## 4. Tests
+
+## 5. Impliment
+
+## 6. Reflect
+
+
+How I solved it:
+
+1. Read problem
+2. researched binary trees https://www.youtube.com/watch?v=fAAZixBzIAI&ab_channel=freeCodeCamp.org
+3. wrote psuedocode for inputs and outputs
+4. defined problem
+5. Took a brain break
+6. 
--- c2-1-ion-flux-relabling/solution.py
+++ c2-1-ion-flux-relabling/solution.py
@@ -1,25 +1,25 @@
 def solution(h, q):
 
-    """Finds the parent of each child in a list of nodes in a perfect binary tree(POT) of height h
+    """Finds the parent of each child node in a list of nodes that belong to a perfect binary tree
 
     Time Complexity:
         O(n log n)
+        
     Parameters:
         h (int): Height of the binary tree
         q (list of int): List of node positions
     
     Returns:
-        parent_nodes (list of int): list of parent node positions, if the node doesnt have a parent, return a -1 instead
-
+        parent_nodes (list of int): List of parent node positions, root nodes return -1
     """
     
     
     parent_nodes = []
 
-    ## find the max number of nodes in a perfect binary tree of height h
+    # find the max number of nodes in a perfect binary tree of height h
     max_nodes = 2**h - 1
 
-    # loop over nodes and return parent node
+    # loop over node list and return parent node
     for node in q:
 
         end_node =  max_nodes
@@ -30,39 +30,25 @@ def solution(h, q):
             parent_nodes.append(-1)
             continue 
 
-        # loop until the parent node is found
+        # loop over tree until the parent node is found
         while (node >= 1):
 
             end_node = end_node -1
 
-            ## find the midpoint of the tree
+            #find the midpoint of the tree
             mid_node = start_node + (end_node - start_node)/2
-
+            
+            #if the node has been found, add it to the parent_nodes list and end the loop 
             if (mid_node == node or end_node == node):
                 parent_nodes.append(end_node + 1)
                 break
-
+            
+            # if the node is less than the mid_node search the left subtree 
             elif(node < mid_node):
                 end_node = mid_node
-            
+                
+            # otherwise, search the the right subtree 
             else:
                 start_node = mid_node
 
     return parent_nodes
-
-
-
-# def test_solution(h, q, expected):
-#     print("RECEIVED:", solution(h, q))
-#     assert solution(h, q) == expected, "Should be {}".format(expected)
-#     print("Test passed!!")
-
-
-       
-
-
-# print("<case 1----------------->")
-# test_solution(3, [7, 3, 5, 1], [-1,7,6,3]  )
-
-# print("<case 2----------------->")
-# test_solution(5, [19, 14, 28], [21,15,29]  )
